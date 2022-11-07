10650806220803743a1f49d61fed6529f4c10f86
Chris <lys000626@gmail.com>
2022-11-07 17:52:30 +0900

    1000.01.00

---
+++ 0x02/1000/1000/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a, b;
+	int sum;
+
+	cin >> a >> b;
+
+	sum = a + b;
+
+	cout << sum;
+
+	return 0;
+}
