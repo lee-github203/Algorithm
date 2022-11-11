f007ec9020fad0e90caa4d6a054cbaa85b11240d
Chris <lys000626@gmail.com>
2022-11-11 19:17:43 +0900

    2446.01.00

---
+++ 0x02/2446/2446/Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	int i;
+	int j;
+	int true_count = 0;
+	int mid = 0;
+
+	cin >> N;
+
+	for (i = 0; i < N * 2 - 1; ++i)
+	{
+		if (i == N)
+		{
+			mid = 1;
+		}
+
+		for (j = 0; j < N * 2 - 1; ++j)
+		{
+			if (mid == 0 && i == j)
+			{
+				true_count = 1;
+			}
+
+			if (mid != 0 && i + j >= (N - 1) * 2)
+			{
+				true_count = 1;
+			}
+
+			if (true_count)
+			{
+				cout << '*';
+			}
+			else
+			{
+				cout << ' ';
+			}
+
+			if (mid == 0 && i + j == (N - 1) * 2 || mid != 0 && j - i == 0)
+			{
+				break;
+			}
+		}
+
+		true_count = 0;
+		cout << '\n';
+	}
+
+	return 0;
+}
