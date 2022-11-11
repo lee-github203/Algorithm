0a526420b74e8baa9bc54c56355f73bd1083f2cc
Chris <lys000626@gmail.com>
2022-11-11 16:44:28 +0900

    2440.01.00

---
+++ 0x02/2440/2440/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	int i;
+	int j;
+
+	cin >> N;
+
+	for (i = 0; i < N; ++i)
+	{
+		for (j = 0; j < N; ++j)
+		{
+			if (i + j < N)
+			{
+				cout << '*';
+			}
+		}
+
+		cout << '\n';
+	}
+
+	return 0;
+}
