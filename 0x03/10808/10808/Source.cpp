9c401bf4dabfbe8a3f7746ccbec982b9793f0c51
Chris <lys000626@gmail.com>
2022-11-12 17:37:56 +0900

    10808.01.00

--- 0x03/10808/10808/Source.cpp
+++ 0x03/10808/10808/Source.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 using namespace std;
+#define ASCII_CODE_LOWER_CASE_A (97)
+#define ARRAY_LENGTH (26)
 
 int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	
+	string S;
+	size_t i;
+	int alphabet_arr[ARRAY_LENGTH] = { 0, };
+	size_t index;
+
+	cin >> S;
+
+	for (i = 0; i < S.size(); ++i)
+	{
+		index = S[i] - ASCII_CODE_LOWER_CASE_A;
+		++alphabet_arr[index];
+	}
+
+	for (i = 0; i < ARRAY_LENGTH; ++i)
+	{
+		cout << alphabet_arr[i] << ' ';
+	}
 
 	return 0;
 }
